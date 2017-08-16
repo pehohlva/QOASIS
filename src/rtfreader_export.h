8cf527fcac037ef656b1d1bda40de0c3e51b427a
Piter K <pehohlva@gmail.com>
2017-08-16 15:06:53 +0200

    append session .... e lib make?

---
+++ src/rtfreader_export.h
@@ -0,0 +1,25 @@
+#ifndef RTFEXPORT_EXTERN_TO_LIB_762134
+#define RTFEXPORT_EXTERN_TO_LIB_762134
+
+#include <QMainWindow>
+#include <QXmlQuery>
+#include <QSystemTrayIcon>
+#include <QAction>
+#include <QDomDocument>
+#include <QAbstractScrollArea>
+#include <QPainter>
+#include <QTextDocument>
+#include <QDebug>
+#include <QTextFrameFormat>
+#include <QTextFrame>
+#include <QBuffer>
+
+
+
+
+
+
+
+
+
+#endif
